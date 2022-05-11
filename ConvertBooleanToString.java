9c801247a5fbd98819ccba369c9bbb74bf294c87
Abrahim Abdulkader <79820328+AbrahimAbdulkader@users.noreply.github.com>
2022-05-11 14:08:32 -0400

    Convert Boolean To String

---
+++ ConvertBooleanToString.java
@@ -0,0 +1,10 @@
+package FundamentalQuestions;
+
+public class ConvertBooleanToString {
+    public static String convert(boolean b) {
+        if (true) {
+            return String.valueOf(b);
+        }
+        return String.valueOf(b);
+    }
+}
