4bc535e0613c8d371ba6c80071dba6169e44da8d
Abrahim Abdulkader <79820328+AbrahimAbdulkader@users.noreply.github.com>
2022-07-16 10:21:39 -0500

    PanagramChecker

---
+++ PangramChecker.java
@@ -0,0 +1,23 @@
+package FundamentalQuestions;
+
+public class PangramChecker {
+
+    /*
+    A pangram is a sentence that contains every single letter of the alphabet at least once.
+    For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it
+    uses the letters A-Z at least once (case is irrelevant).
+
+    Given a string, detect whether or not it is a pangram. Return True if it is, False if not.
+    Ignore numbers and punctuation.
+     */
+
+    public static boolean check(String sentence) {
+        boolean result = sentence.matches("[a-zA-Z]+");
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(check("The quick brown fox jumps over the lazy dog"));
+    }
+}
